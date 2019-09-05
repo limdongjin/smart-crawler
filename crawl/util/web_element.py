52b83839ef767e4712075e543867731c0e07fc30
imdongjin <geniuslim27@gmail.com>
2019-09-06 02:58:14 +0900

    refactoring: sub function of parse_table

--- crawl/util/web_element.py
+++ crawl/util/web_element.py
@@ -6,21 +6,19 @@ def to_texts(_: List[WebElement]) -> List[str]:
     return list(map(lambda _: _.text, _))
 
 
-def _find_head_texts(table: WebElement) -> List[str]:
+def _parse_ths(table: WebElement) -> List[str]:
     return to_texts(table.find_elements_by_tag_name('th'))
 
 
-def _find_rows(table: WebElement) -> List[WebElement]:
-    return table.find_elements_by_tag_name('tr')
-
-
-def _find_td_texts(row: WebElement) -> List[str]:
-    return to_texts(row.find_elements_by_tag_name('td'))
-
-
-def _find_datas(table: WebElement) -> List[List[str]]:
+def _parse_tds(table: WebElement) -> List[List[str]]:
+    _find_td_texts: Callable[[WebElement], List[str]] = (
+        lambda _: to_texts(_.find_elements_by_tag_name('td'))
+    )
+    _find_rows: Callable[[WebElement], List[WebElement]] = (
+        lambda _: _.find_elements_by_tag_name('tr')
+    )
     return list(map(_find_td_texts, _find_rows(table)))
 
 
 def parse_table(table: WebElement) -> List[Dict[str, str]]:
-    return list(map(lambda data: dict(zip(_find_head_texts(table), data)), _find_datas(table)))
+    return list(map(lambda data: dict(zip(_parse_ths(table), data)), _parse_tds(table)))
