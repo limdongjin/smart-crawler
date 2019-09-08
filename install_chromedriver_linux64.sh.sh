3b2d85da4e6e58b1ffca2b90ef9b3c88c67a3ee8
imdongjin <geniuslim27@gmail.com>
2019-09-09 07:58:12 +0900

    etc(install__.sh): chrome,chromedriver install shell script for linux

---
+++ install_chromedriver_linux64.sh.sh
@@ -0,0 +1,3 @@
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
