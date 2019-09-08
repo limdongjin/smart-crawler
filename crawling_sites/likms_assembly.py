eb018b77ac6559e1647695f9803260486e26e746
imdongjin <geniuslim27@gmail.com>
2019-09-09 03:01:46 +0900

    features(crawling_moorings): add crawl proponents feature
    법안 발의자도 크롤링하도록 기능 추가

--- crawling_sites/likms_assembly.py
+++ crawling_sites/likms_assembly.py
@@ -5,6 +5,7 @@ from util.common import merge_dict, to_strips
 from crawl.parse.xpath import parse_by_xpath
 from selenium.common.exceptions import JavascriptException
 from bs4 import BeautifulSoup
+import re
 import time
 
 
@@ -41,10 +42,9 @@ def crawlling_moorings(page=1):
                 return []
             time.sleep(5)
 
-    get_PRC_XXX = lambda mooring: mooring['links'][0]['href'].split("'")[1]
-
     for mooring in moorings:
-        mooring['url'] = 'http://likms.assembly.go.kr/bill/billDetail.do?billId=' + get_PRC_XXX(mooring)
+        mooring['prc'] = _get_prc_xxx(mooring)
+        mooring['url'] = 'http://likms.assembly.go.kr/bill/billDetail.do?billId=' + mooring['prc']
 
     res = list(map(lambda _: merge_dict(_, _crawl_bill(driver, _['url'])), moorings))
 
@@ -81,4 +81,56 @@ def _crawl_bill(driver, url: str):
                                                      '//*[@id="summaryContentDiv"]/text()'))))
     res['summaries'] = summaries
 
+    res['proponents'] = _crawl_proponents(driver, url)
+
     return res
+
+
+# 법안 발의자
+def _crawl_proponents(driver, detail_url: str):
+    url = 'http://likms.assembly.go.kr/bill/coactorListPopup.do?billId='+_get_prc_xxx(detail_url)
+    driver.get(url)
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source)
+
+    soup = soup.find('div', class_='links textType02 mt20')
+    a_tags = soup.find_all('a')
+
+    proponents = []
+    for a in a_tags:
+        proponent = dict()
+
+        proponent['url'] = a.attrs['href']
+
+        # ex) a.text = '이장우(새누리당/李莊雨)'
+        (proponent['한글이름'],
+         proponent['정당'],
+         proponent['한자이름']) = tuple(re.split('[(:/:)]', a.text)[:-1])
+        proponent['유형'] = '공동발의자'
+
+        proponents.append(proponent)
+
+    bill_title = parse_by_xpath(page_source, '//*[@id="periodDiv"]/div[2]/p/text()')[0]
+
+    # ex)  '[2022357]2018년도 산업통상자원... 감사요구안(산업통상자원중소벤처기업위원장)'
+    # ex2) [2022392]선박직원법 일부개정법률안(황주홍의원 등 13인)
+    main_proponent = re.split('[(:)]', bill_title)[-2]
+    if main_proponent.find('의원 등'):
+        main_proponent = main_proponent.split('의원 등')[0]
+        for proponent in proponents:
+            if proponent['한글이름'] == main_proponent:
+                proponent['유형'] = '대표발의자'
+    else:
+        proponents.append({'한글이름': main_proponent,
+                           '정당': '',
+                           '한자이름': '',
+                           '유형': main_proponent})
+
+    return proponents
+
+
+def _get_prc_xxx(mooring):
+    if type(mooring) is str:
+        return mooring.split('billId=')[1]
+    return mooring['links'][0]['href'].split("'")[1]
