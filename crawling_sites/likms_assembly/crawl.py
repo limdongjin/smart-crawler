fe40fdaab1809e343b0ea045c8a063ee914aa29b
imdongjin <geniuslim27@gmail.com>
2019-09-12 19:16:37 +0900

    enhancement(likms_assembly): 구조화 및 로직 개선

--- crawling_sites/likms_assembly/crawl.py
+++ crawling_sites/likms_assembly/crawl.py
@@ -1,135 +1,136 @@
-
 import re
-import time
-from selenium.common.exceptions import JavascriptException
 from bs4 import BeautifulSoup
+
+from crawl.connect_and_execute_script import ConnectAndExecuteScript
 from crawl.parse.table import parse_tables
 from crawl.drivers_manager import DriversManager
 from util.common import merge_dict, to_strips
 from crawl.parse.xpath import parse_by_xpath
+import requests
 
 
-def crawling_moorings(page=1):
-    driver = DriversManager().drivers[page % 8]
+class Crawl:
+    @classmethod
+    def detail_page(cls, mooring):
+        return merge_dict(mooring, _CrawlBills.detail_page(mooring['url']))
 
-    moorings_base_url: str = "http://likms.assembly.go.kr/bill/MooringBill.do"
-    moorings = []
+    @classmethod
+    def list_page(cls, page):
+        return _CrawlBills(page, 'mooring').list_page()
 
-    for i in range(4):
+
+class _CrawlBills:
+    def __init__(self, page, category):
+        self.page = page
+        self.driver = DriversManager().drivers[page % 8]
+        self.category = category
+        self.url = "http://likms.assembly.go.kr/bill/MooringBill.do" if category == 'mooring' else None
+
+    def list_page(self):
+        script = 'javascript:GoPage({0})'.format(self.page) if self.page != 1 else None
+        page_source = ConnectAndExecuteScript(self.driver).run(url=self.url,
+                                                               script=script,
+                                                               max_repeat=4)
         try:
-            if i > 0:
-                print("re-try connect page={0}, n={1}".format(page, i))
-            driver.get(moorings_base_url)
-            if page != 1:
-                driver.execute_script("javascript:GoPage({0})".format(page))
-            if i > 0:
-                print('success')
-            page_source = driver.page_source
+            moorings = parse_tables(page_source)[0]['rows']
+        except IndexError as e:
+            print(e)
+            # print(moorings)
+            print(page_source)
+            print(self.driver.page_source)
+            print(type(page_source))
+            return []
 
-            moorings = parse_tables(page_source)
-            moorings = moorings[0]['rows']
+        for mooring in moorings:
+            mooring['prc'] = self._get_prc_xxx(mooring)
+            mooring['url'] = 'http://likms.assembly.go.kr/bill/billDetail.do?billId=' + mooring['prc']
 
-            break
-        except JavascriptException:
-            if i == 3:
-                print('fail')
-                return []
-            time.sleep(5)
-        except IndexError:
-            if i == 3:
-                print('fail')
-                return []
-            time.sleep(5)
+        return moorings
 
-    for mooring in moorings:
-        mooring['prc'] = _get_prc_xxx(mooring)
-        mooring['url'] = 'http://likms.assembly.go.kr/bill/billDetail.do?billId=' + mooring['prc']
+    @classmethod
+    def detail_page(cls, url):
+        res = {}
+        page_source = requests.get(url).text
+        # driver.get(url)
+        #
+        # page_source = driver.page_source
 
-    res = list(map(lambda _: merge_dict(_, _crawl_bill(driver, _['url'])), moorings))
+        soup = BeautifulSoup(page_source, features='html.parser')
 
-    return res
+        # 심사 진행 단계
+        res['status'] = soup.find('span', class_='on').text
+        # print(res['status'].text)
 
+        tables = parse_tables(page_source)
+        # tables[0] : 의안 접수 정보 테이블
+        # tables[1] : 위원회 접수 테이블
+        # ... tables[x]['attrs']['summary'] 로 테이블 이름 확인 가능
+        # ... tables[x]['rows'] 로 데이터 접근
 
-def _crawl_bill(driver, url: str):
-    res = {}
-    driver.get(url)
+        valid_info = lambda _: _['attrs'] != {} and _['attrs']['summary'] != '등록의견 리스트의 번호, 제목, 작성자, 의견제출기관, 등록일 정보'
+        res['informationTables'] = list(filter(valid_info, tables))
+        # res['informationTables'] = tables
+        # print(res)
 
-    page_source = driver.page_source
+        # 제안이유 및 주요내용
+        # ['제안이유', 'bla', 'bla']
+        summaries = list(filter(lambda _: _,
+                                to_strips(parse_by_xpath(page_source,
+                                                         '//*[@id="summaryContentDiv"]/text()'))))
+        res['summaries'] = summaries
 
-    soup = BeautifulSoup(page_source, features='html.parser')
+        res['proponents'] = cls._crawl_proponents(url)
+        print(res)
+        return res
 
-    # 심사 진행 단계
-    res['status'] = soup.find('span', class_='on').text
-    # print(res['status'].text)
+    @classmethod
+    def _crawl_proponents(cls, detail_url: str):
+        url = 'http://likms.assembly.go.kr/bill/coactorListPopup.do?billId=' + cls._get_prc_xxx(detail_url)
+        # driver.get(url)
+        #
+        # page_source = driver.page_source
 
-    tables = parse_tables(page_source)
-    # tables[0] : 의안 접수 정보 테이블
-    # tables[1] : 위원회 접수 테이블
-    # ... tables[x]['attrs']['summary'] 로 테이블 이름 확인 가능
-    # ... tables[x]['rows'] 로 데이터 접근
+        page_source = requests.get(url).text
 
-    valid_info = lambda _: _['attrs'] != {} and _['attrs']['summary'] != '등록의견 리스트의 번호, 제목, 작성자, 의견제출기관, 등록일 정보'
-    res['informationTables'] = list(filter(valid_info, tables))
-    # res['informationTables'] = tables
-    # print(res)
+        soup = BeautifulSoup(page_source, features='html.parser')
 
-    # 제안이유 및 주요내용
-    # ['제안이유', 'bla', 'bla']
-    summaries = list(filter(lambda _: _,
-                            to_strips(parse_by_xpath(page_source,
-                                                     '//*[@id="summaryContentDiv"]/text()'))))
-    res['summaries'] = summaries
+        soup = soup.find('div', class_='links textType02 mt20')
+        a_tags = soup.find_all('a')
+        # print(a_tags)
+        proponents = []
+        for a_tag in a_tags:
+            proponent = dict()
 
-    res['proponents'] = _crawl_proponents(driver, url)
+            proponent['url'] = a_tag.attrs['href']
 
-    return res
+            # ex) a_tag.text = '이장우(새누리당/李莊雨)'
+            (proponent['한글이름'],
+             proponent['정당'],
+             proponent['한자이름']) = tuple(re.split('[(:/:)]', a_tag.text)[:-1])
+            proponent['유형'] = '공동발의자'
 
+            proponents.append(proponent)
 
-# 법안 발의자
-def _crawl_proponents(driver, detail_url: str):
-    url = 'http://likms.assembly.go.kr/bill/coactorListPopup.do?billId='+_get_prc_xxx(detail_url)
-    driver.get(url)
+        bill_title = parse_by_xpath(page_source, '//*[@id="periodDiv"]/div[2]/p/text()')[0]
 
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, features='html.parser')
+        # ex)  '[2022357]2018년도 산업통상자원... 감사요구안(산업통상자원중소벤처기업위원장)'
+        # ex2) [2022392]선박직원법 일부개정법률안(황주홍의원 등 13인)
+        main_proponent = re.split('[(:)]', bill_title)[-2]
+        if main_proponent.find('의원 등'):
+            main_proponent = main_proponent.split('의원 등')[0]
+            for proponent in proponents:
+                if proponent['한글이름'] == main_proponent:
+                    proponent['유형'] = '대표발의자'
+        else:
+            proponents.append({'한글이름': main_proponent,
+                               '정당': '',
+                               '한자이름': '',
+                               '유형': main_proponent})
+        # print(proponents)
+        return proponents
 
-    soup = soup.find('div', class_='links textType02 mt20')
-    a_tags = soup.find_all('a')
-    # print(a_tags)
-    proponents = []
-    for a_tag in a_tags:
-        proponent = dict()
-
-        proponent['url'] = a_tag.attrs['href']
-
-        # ex) a_tag.text = '이장우(새누리당/李莊雨)'
-        (proponent['한글이름'],
-         proponent['정당'],
-         proponent['한자이름']) = tuple(re.split('[(:/:)]', a_tag.text)[:-1])
-        proponent['유형'] = '공동발의자'
-
-        proponents.append(proponent)
-
-    bill_title = parse_by_xpath(page_source, '//*[@id="periodDiv"]/div[2]/p/text()')[0]
-
-    # ex)  '[2022357]2018년도 산업통상자원... 감사요구안(산업통상자원중소벤처기업위원장)'
-    # ex2) [2022392]선박직원법 일부개정법률안(황주홍의원 등 13인)
-    main_proponent = re.split('[(:)]', bill_title)[-2]
-    if main_proponent.find('의원 등'):
-        main_proponent = main_proponent.split('의원 등')[0]
-        for proponent in proponents:
-            if proponent['한글이름'] == main_proponent:
-                proponent['유형'] = '대표발의자'
-    else:
-        proponents.append({'한글이름': main_proponent,
-                           '정당': '',
-                           '한자이름': '',
-                           '유형': main_proponent})
-
-    return proponents
-
-
-def _get_prc_xxx(mooring):
-    if isinstance(mooring, str):
-        return mooring.split('billId=')[1]
-    return mooring['links'][0]['href'].split("'")[1]
+    @classmethod
+    def _get_prc_xxx(cls, mooring):
+        if isinstance(mooring, str):
+            return mooring.split('billId=')[1]
+        return mooring['links'][0]['href'].split("'")[1]
