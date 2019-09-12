74f0099f11f0f86b54810cbbe919ddcb9ba63e94
imdongjin <geniuslim27@gmail.com>
2019-09-12 19:17:23 +0900

    test(likms_assembly): test 코드 변경

--- tests/test_likms_assembly.py
+++ tests/test_likms_assembly.py
@@ -1,36 +1,11 @@
-import time
-from multiprocessing import Pool
 from unittest import TestCase
 
-from crawl.drivers_manager import DriversManager
-from crawling_sites.likms_assembly.crawl import crawling_moorings
-from util.common import flatten
+from crawling_sites.likms_assembly.main import LikmsAssembly
 
 
 class TestLikmsAssembly(TestCase):
     def test_crawling(self):
-        start_time = time.time()
-        res = []
+        res = LikmsAssembly.moorings(1, 100)
 
-        driver_manager = DriversManager()
-        for i in range(0, 8):
-            driver_manager.create()
-        print("--- %s seconds ---" % (time.time() - start_time))
-
-        process_num = 8
-        pool = Pool(processes=process_num)
-
-        for start in range(1, 10, process_num):
-            res.extend(pool.map(crawling_moorings, range(start, start + process_num)))
-            for i in range(start, start + 8):
-                print("page {0} clear".format(i))
-            print("--- %s seconds ---" % (time.time() - start_time))
-
-        print("--- %s seconds ---" % (time.time() - start_time))
-
-        res = flatten(res)
+        print('res!')
         print(res)
-
-        driver_manager.destroy_myself()
-        pool.close()
-        pool.join()
