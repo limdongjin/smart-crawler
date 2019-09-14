7a6c62fb29b0b6d1e923b2fd8db0aee16a38e825
imdongjin <geniuslim27@gmail.com>
2019-09-15 01:29:21 +0900

    enhancement(DriverManager): add configuration options for enhance performance

--- crawl/drivers_manager.py
+++ crawl/drivers_manager.py
@@ -47,4 +47,24 @@ class DriversManager(metaclass=Singleton):
             self.chrome_options.add_argument('--headless')
             self.chrome_options.add_argument('--no-sandbox')
             self.chrome_options.add_argument('--disable-dev-shm-usage')
+        self.chrome_options.add_argument("disable-gpu")
         self.executable_path = conf['chrome_driver_binary']
+
+        prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 1,
+                                                            'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                            'notifications': 2, 'auto_select_certificate': 2,
+                                                            'fullscreen': 2,
+                                                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                            'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                            'protocol_handlers': 2,
+                                                            'ppapi_broker': 2, 'automatic_downloads': 2,
+                                                            'midi_sysex': 2,
+                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                            'metro_switch_to_   desktop': 2,
+                                                            'protected_media_identifier': 2, 'app_banner': 2,
+                                                            'site_engagement': 2,
+                                                            'durable_storage': 2}}
+        self.chrome_options.add_experimental_option('prefs', prefs)
+        self.chrome_options.add_argument("start-maximized")
+        self.chrome_options.add_argument("disable-infobars")
+        self.chrome_options.add_argument("--disable-extensions")
