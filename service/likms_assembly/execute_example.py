d41294ebba75bdad661750e1d000213a766aaa45
imdongjin <geniuslim27@gmail.com>
2019-09-17 01:17:27 +0900

    feature(store.dynamodb): DynamoTable object

--- service/likms_assembly/execute_example.py
+++ service/likms_assembly/execute_example.py
@@ -1,8 +1,10 @@
-from store.dynamodb import put_items
 from service.likms_assembly.main import LikmsAssemblyService
+from store.dynamodb import DynamoTable
+
 def main():
-    # 의안정보시스템의 1페이지부터 30페이지까지 크롤링함.
-    res = LikmsAssemblyService.moorings(1, 30)
+    # 의안정보시스템의 1페이지부터 1000페이지까지 크롤링함.
+    res = LikmsAssemblyService.moorings(1, 1000)
 
     # dynamodb 에 결과를 저장함
-    put_items('bills', res)
+    bills = DynamoTable('bills')
+    bills.put_items(res)
--- store/dynamodb.py
+++ store/dynamodb.py
@@ -1,26 +1,31 @@
 import boto3
 
 
-def get_dynamodb():
-    return boto3.resource('dynamodb')
+class DynamoTable:
+    def __init__(self, table_name):
+        self._resource_dynamodb = boto3.resource('dynamodb')
+        self._table = self._resource_dynamodb.Table(table_name)
+        self.items = None
 
+    def get_all_items(self, cached=True):
+        if cached is True and self.items is not None:
+            return self.items
 
-def get_table(table_name):
-    dynamodb = get_dynamodb()
-    return dynamodb.Table(table_name)
+        res = self._table.scan()
+        data = res['Items']
+        while 'LastEvaluatedKey' in res:
+            res = self._table.scan(ExclusiveStartKey=res['LastEvaluatedKey'])
+            data.extend(res['Items'])
 
+        self.items = data
+        return data
 
-def put(table_name, item, table=None):
-    if not table:
-        table = get_table(table_name)
-    return table.put_item(Item=item)
+    def put(self, item):
+        return self._table.put_item(Item=item)
 
+    def put_items(self, items):
+        with self._table.batch_writer() as batch:
+            for item in items:
+                batch.put_item(Item=item)
 
-def put_items(table_name, items, table=None):
-    if not table:
-        table = get_table(table_name=table_name)
-    with table.batch_writer() as batch:
-        for item in items:
-            batch.put_item(Item=item)
-
-    return True
+        return True
